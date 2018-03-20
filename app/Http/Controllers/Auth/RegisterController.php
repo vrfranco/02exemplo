abc93974a59bdceb2a8bcfc2f3c452384e30330e
Vinicius R. Franco <vrfrnco@gmail.com>
2018-03-20 13:31:35 -0400

    Exemplo de Cadastro e Suporte ao AWS Worker

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Mail;
+use App\Mail\Teste;
 
 class RegisterController extends Controller
 {
@@ -63,10 +65,14 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $user = User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+        
+        Mail::to( $user )->send( new Teste() );
+
+        return $user;
     }
 }
