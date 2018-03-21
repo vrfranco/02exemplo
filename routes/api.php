40bc3323d1041f3f7a8c3fd95657367118fb7e0c
Vinicius R. Franco <vrfrnco@gmail.com>
2018-03-21 01:46:06 -0400

    Testando

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
-| API Routes
+| API Routes :)
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
